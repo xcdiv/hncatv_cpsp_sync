babe7f5c95a28e6b9da8ab1e30f4661f8a240323
huanan <cdiv@qq.com>
2018-04-15 20:39:10 +0800

    null data
    null data

---
+++ dy_api/api.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="api.aspx.cs" Inherits="dy_api_api" %>
---
+++ static_api/123001/cp.js
@@ -0,0 +1,212 @@
+﻿[
+ { 
+"guid": "651123651as65a651sd6a651sa"
+,"spcode":"123145", "title": "环太平洋2"
+ 
+ }
+, {
+    "guid": "235f12651f6215f65123"
+    , "spcode": "123145", "title": "环太平洋1"
+}
+]
+
+[
+    //235f12651f6215f65123.json
+    {
+        "guid": "235f12651f6215f65123"
+
+        , "spcode": "123145"
+        , "title": "环太平洋1"
+        , "title2": "机器人大战外星小怪兽"
+        , "type": "movie"
+        , "parent_guid": ""
+        , "no":1
+        , "num": 1
+        , "class": ["电影", "科幻"]
+        , "actor": ["刘德华", "王晶"]
+        , "full_note": "2013年，环太平洋地区海底深处出现了一个平行宇宙“突破点”，随后，一个巨大无比的巨兽生物从“海洋”中崛起，第一只巨兽首先摧毁了旧金山以及周围所有的海岸城市，而人类大部分企图阻止巨兽的军事行动全部以失败告终，但巨兽也最终在人类的殊死抵抗下死于核弹中，但旧金山方圆百里变成了荒芜之地。事件结束后，人类为巨兽起了“怪兽”（Kaiju）的名字，但原本认为结束的怪兽袭击却接二连三而出现在各个环太平洋城市。人类在无计可施之下发明了他们自己创造的“怪兽”：“机甲猎人”（Jaeger），利用巨大机械士兵来对抗怪兽大军，由两名脑部神经网络互相串连（浮动神经元连结）的操纵者同步操作战斗机械士兵，利用仪器来检测怪兽的级别与代号。自从有了机甲猎人，人类开始获得胜利，甚至将怪兽袭击变成宣传活动与仪式，但人类却并不知道更加恐怖的威胁即将来临，而“怪兽战役”也就此打响。<br>2020年2月28日，代号“镰刀头”的三级怪兽出现在阿拉斯加海域，贝克特兄弟罗利.贝克特（查理·汉纳姆饰）和杨希.贝克特驾驶危险流浪者迎击怪兽，战斗中哥哥彦希不幸牺牲，罗利独自一人驾驶几乎报废的危险流浪者返回。  "
+        , "createtime": "2018-01-23 11:01:11"
+        , "updatetime": "2018-01-23 11:01:11"
+        , "droptime": "2028-01-23 11:01:11"
+        , "sort": 1000
+        , "player":"OTT"
+        , "img": {
+            "poster": [
+                { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+            ]
+        , "stage": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+        ]
+         , "background ": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+         ]
+        }
+        , "url": {
+            "href": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+        }
+    }
+
+    ,
+    //文件名：a4s5f5a611565a1sfasf.json
+    {
+        "guid": "a4s5f5a611565a1sfasf"
+
+        , "spcode": "123145"
+        , "title": "百家讲坛"
+        , "title2": "大师荟萃"
+        , "type": "television"
+        , "parent_guid": ""
+        , "no":1
+       
+        , "class": ["综艺", "科教","国学"]
+        , "actor": ["王立群", "袁腾飞"]
+        , "full_note": "《百家讲坛》栏目一贯坚持“让专家、学者为百姓服务”的栏目宗旨，栏目在专家、学者和百姓之间架起一座桥梁“一座让专家通向老百姓的桥梁”，从而达到普及优秀中国传统文化的目的。 "
+        , "createtime": "2018-01-23 11:01:11"
+        , "updatetime": "2018-01-23 11:01:11"
+        , "droptime": "2028-01-23 11:01:11"
+        , "sort": 1000
+                , "player": "OTT"
+        , "img": {
+            "poster": [
+                { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+            ]
+        , "stage": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+        ]
+         , "background ": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+         ]
+        }
+        , "url": {
+            "href": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+             
+        }
+    }
+
+      ,
+    //文件名：56as1da56s1d56as1d65a1sd.json
+    {
+        "guid": "56as1da56s1d56as1d65a1sd"
+
+        , "spcode": "123145"
+        , "title": "百家讲坛"
+        , "title2": "大宋风云-第二期"
+        , "type": "television"
+        , "parent_guid": "a4s5f5a611565a1sfasf"
+        , "no": 23
+        , "class": ["综艺", "科教", "国学"]
+        , "actor": [ "袁腾飞"]
+        , "full_note": "大宋风云-第二期"
+        , "createtime": "2018-01-23 11:01:11"
+        , "updatetime": "2018-01-23 11:01:11"
+        , "droptime": "2028-01-23 11:01:11"
+        , "sort": 1000
+                , "player": "OTT"
+        , "img": {
+            "poster": [
+                { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+            ]
+        , "stage": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+        ]
+         , "background ": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+         ]
+        }
+        , "url": {
+            "href": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+        }
+    }
+
+
+      ,
+    //文件名：56as1da56s1d56as1d65a1sd.json
+    {
+        "guid": "56as1da56s1d56as1d65a1sd"
+
+        , "spcode": "123145"
+        , "title": "百家讲坛"
+        , "title2": "大宋风云-第二期"
+        , "type": "television"
+        , "parent_guid": "a4s5f5a611565a1sfasf"
+        , "no": 23
+        , "class": ["综艺", "科教", "国学"]
+        , "actor": ["袁腾飞"]
+        , "full_note": "大宋风云-第二期"
+        , "createtime": "2018-01-23 11:01:11"
+        , "updatetime": "2018-01-23 11:01:11"
+        , "droptime": "2028-01-23 11:01:11"
+        , "sort": 1000
+                , "player": "OTT"
+        , "img": {
+            "poster": [
+                { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+            ]
+        , "stage": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+        ]
+         , "background ": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+         ]
+        }
+        , "url": {
+            "href": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+        }
+    }
+    ,
+
+       {
+           "guid": "56as1da56s1d56as1d65a1sd" 
+        , "spcode": "123145"
+        , "title": "2017欧美超级大片"
+        , "title2": "动作大片荟萃"
+        , "type": "topic"
+        , "parent_guid": ""
+        , "no": 23
+        , "class": ["电影", "动作", "战争"]
+        , "actor": ["汤姆.克鲁斯"]
+        , "full_note": "大宋风云-第二期"
+        , "createtime": "2018-01-23 11:01:11"
+        , "updatetime": "2018-01-23 11:01:11"
+        , "droptime": "2028-01-23 11:01:11"
+        , "sort": 1000
+        , "player": "OTT"
+        , "img": {
+            "poster": [
+                { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+            ]
+        , "stage": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+        ]
+         , "background ": [
+            { "url": "http://cp/res/1.png", "size": "128x128" }, { "url": "http://cp/res/2.jpg", "size": "512x128" }
+         ]
+        }
+        , "url": {
+            "href": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "href4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player09": "http://cp/play.php?id=123123&pgt=#PGT#"
+            , "player4k": "http://cp/play.php?id=123123&pgt=#PGT#"
+        }
+       }
+
+]
